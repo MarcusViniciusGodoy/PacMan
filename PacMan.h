fdc4a38b8923ebc7c4c888891a180fed6a7fc099
Marcus <marcus@marcus-HP-Pavilion-14-Notebook-PC>
2024-05-22 18:01:16 -0300

    Created struct and function acabou, imprime_mapa, move

--- PacMan.h
+++ PacMan.h
@@ -1,3 +1,12 @@
 void libera_mapa();
 void aloca_mapa();
 void le_mapa();
+void move(char direcao);
+int acabou();
+void imprime_mapa();
+
+struct mapa {
+   char** matriz;
+   int linhas;
+   int colunas;
+}
